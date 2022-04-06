0ca9113b6bf8c8714ab3570f3c8f86e9a1510eaa
Wajjdi <wajdi.gaiech@etu.unice.fr>
2022-04-06 21:41:58 +0200

    parallel graph

--- parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
+++ parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
@@ -1,95 +1,106 @@
 import { Element, Component, Host, Prop, h } from '@stencil/core';
+import { select } from 'd3-selection';
+import { pie, arc } from 'd3-shape';
+
+import { scaleOrdinal } from 'd3-scale';
+import { quantize } from 'd3-interpolate';
+import { interpolateCool } from 'd3-scale-chromatic';
 import * as d3 from "d3";
 @Component({
   tag: 'parallel-graph',
   styleUrl: 'parallel-graph.css',
-  shadow: true,
+  shadow: true
 })
-export class ParallelGraph {
+export class MyComponent {
   @Element() element: HTMLElement;
-  @Prop() width: number = 400;
-  @Prop() height: number = 400;
+  @Prop() width: number = 6000;
+  @Prop() height: number = 6000;
   @Prop() data: string = "[]";
 
+  public chartData: any;
 
- 
-  buildParallelGraph(svg){
-    var margin = {top: 30, right: 10, bottom: 10, left: 0},
-    width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
   
-  // append the svg object to the body of the page
-  var svg = d3.select(this.element.shadowRoot.querySelectorAll("#my_dataviz")[0])
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+
+  componentDidLoad(){
+    let svg = select(this.element.shadowRoot.querySelectorAll(".chart")[0])
+      .attr("width", this.width)
+      .attr("height", this.height);
+    this.buildParalleGraph(svg);
+    
+  }
+
+  
+  buildParalleGraph(svg){
+    var margin = {top: 10, right: 10, bottom: 10, left: 0},
+    width = 1000 - margin.left - margin.right,
+    height = 1000 - margin.top - margin.bottom;
+
+    svg.append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
   
   // Parse the Data
-  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) {
-  
-    // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
-    const dimensions = d3.keys(data[0]).filter(function(d) { return d != "Species" })
-  
-    // For each dimension, I build a linear scale. I store all in a y object
-    var y = {}
-    let i 
-    for (i in dimensions) {
-     const name = dimensions[i]
-      y[name] = d3.scaleLinear()
-        .domain( d3.extent(data, function(d) { return +d[name]; }) )
-        .range([height, 0])
-    }
-  
-    // Build the X scale -> it find the best position for each Y axis
-   const x = d3.scalePoint()
-      .range([0, width])
-      .padding(1)
-      .domain(dimensions);
-  
-    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-    function path(d) {
-        return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
-    }
-  
-    // Draw the lines
-    svg
-      .selectAll("myPath")
-      .data(data)
-      .enter().append("path")
-      .attr("d",  path)
-      .style("fill", "none")
-      .style("stroke", "#69b3a2")
-      .style("opacity", 0.5)
-  
-    // Draw the axis:
-    svg.selectAll("myAxis")
-      // For each dimension of the dataset I add a 'g' element:
-      .data(dimensions).enter()
-      .append("g")
-      // I translate this element to its right position on the x axis
-      .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-      // And I build the axis with the call function
-      .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-      // Add axis title
-      .append("text")
-        .style("text-anchor", "middle")
-        .attr("y", -9)
-        .text(function(d) { return d; })
-        .style("fill", "black")
-  
-  })
+  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then( function(data) {
+
+  // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+ const dimensions = Object.keys(data[0]).filter(function(d) { return d != "Species" })
+
+  // For each dimension, I build a linear scale. I store all in a y object
+  const y = {}
+  for (var i in dimensions) {
+    const name = dimensions[i]
+    y[name] = d3.scaleLinear()
+      .domain( d3.extent(data, function(d) { return +d[name]; }) )
+      .range([height, 20])
   }
 
+  // Build the X scale -> it find the best position for each Y axis
+ const x = d3.scalePoint()
+    .range([0, width])
+    .padding(1)
+    .domain(dimensions);
+
+  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+  function path(d) {
+      return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+  }
+
+  // Draw the lines
+  svg
+    .selectAll("myPath")
+    .data(data)
+    .join("path")
+    .attr("d",  path)
+    .style("fill", "none")
+    .style("stroke", "#69b3a2")
+    .style("opacity", 0.5)
+
+  // Draw the axis:
+  svg.selectAll("myAxis")
+    // For each dimension of the dataset I add a 'g' element:
+    .data(dimensions).enter()
+    .append("g")
+    // I translate this element to its right position on the x axis
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+    // And I build the axis with the call function
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    // Add axis title
+    .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", 10)
+      .text(function(d) { return d; })
+      .style("fill", "black")
+
+})
+  }
 
 
   render() {
-    return (
+    return  (
       <Host>
-        <svg id="my_dataviz"></svg>
+        <h1>Parallel graph</h1>
+        <svg class="chart"/>
       </Host>
-    );
+    )
   }
-
 }
