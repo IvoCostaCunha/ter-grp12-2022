9737435c30bd5bbe9b2043cb35371619a9008891
Wajjdi <wajdi.gaiech@etu.unice.fr>
2022-04-21 12:03:29 +0200

    diagram with database wasaby but little problem in the data

--- parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
+++ parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
@@ -8,48 +8,48 @@ import * as d3 from "d3";
 })
 export class MyComponent {
   @Element() element: HTMLElement;
-  @Prop() width: number = 1000 ;
+  @Prop() width: number = 1000;
   @Prop() height: number = 1000;
   @Prop() data: string = "[]";
 
   @Prop() artist;
 
   // Constain valuable information to use in the parallel graph
-  @State() dataObj = {artist:""};
+  @State() dataObj = { artist: "" };
   @State() dataObjInt;
-
+  @State() dataa;
   public chartData: any;
 
-  
 
-  componentDidLoad(){
+
+  componentDidLoad() {
+    this.analiseData();
+    this.convertLengthToInt();
+    this.getSongs(this.dataObj);
     let svg = select(this.element.shadowRoot.querySelectorAll(".chart")[0])
       .attr("width", this.width)
       .attr("height", this.height);
     this.buildParalleGraph(svg);
 
-    this.analiseData();
-    this.convertLengthToInt();
-    this.getSongs(this.dataObj);
-    this.getDataPhaseA(this.dataObj);
-    
+
+
   }
 
   private httpGet(url: string): string {
     let xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", url, false); 
+    xmlHttpReq.open("GET", url, false);
     xmlHttpReq.send(null);
     return xmlHttpReq.responseText;
   }
   private getData(): string {
-    try{
-      let data = this.httpGet("https://wasabi.i3s.unice.fr/api/v1/artist_all/name/"+this.artist)
+    try {
+      let data = this.httpGet("https://wasabi.i3s.unice.fr/api/v1/artist_all/name/" + this.artist)
       return data;
     }
-    catch(error) {
+    catch (error) {
       console.log(error);
       alert("Data not received due to : " + error);
-    } 
+    }
   }
 
   private analiseData() {
@@ -65,14 +65,14 @@ export class MyComponent {
       let globalGenre = album["genre"];
 
       songs.forEach(song => {
-        let songObj = {id:"", title:"", length:"", language:"", isClassic:""};
+        let songObj = { id: "", title: "", length: "", language: "", isClassic: "" };
 
         songObj.id = song["_id"];
         songObj.title = song["title"];
         songObj.isClassic = song["isClassic"];
 
         // We test if lenght is defined 
-        if(song["length"] != "") {
+        if (song["length"] != "") {
           songObj.length = song["length"];
         }
         else {
@@ -80,7 +80,7 @@ export class MyComponent {
         }
 
         // We test if genre is defined in the song otherwise we use album genre
-        if(song.hasOwnProperty("genre")) {
+        if (song.hasOwnProperty("genre")) {
           let genreArray = song["genre"];
           let genre = {};
           let i = 0;
@@ -90,8 +90,8 @@ export class MyComponent {
           });
           songObj["genre"] = genre;
         }
-        else{
-          if(globalGenre != "") {
+        else {
+          if (globalGenre != "") {
             songObj["genre"] = globalGenre;
           }
           else {
@@ -100,7 +100,7 @@ export class MyComponent {
         }
 
         // We test if format is defined otherwise we set it undefined
-        if(song.hasOwnProperty("format")) {
+        if (song.hasOwnProperty("format")) {
           let formatArray = song["format"];
           let format = {};
           let i = 0;
@@ -108,15 +108,15 @@ export class MyComponent {
             format[i] = f;
             i++;
           });
-          songObj["format"]  = format;
+          songObj["format"] = format;
         }
         else {
           songObj["format"] = undefined;
         }
 
         // We verify if "language" isn't undefined, if it is we take "language_detect" instead
-        if(song["language"] == "") {
-          if(song["language_detect"] != undefined) {
+        if (song["language"] == "") {
+          if (song["language_detect"] != undefined) {
             songObj.language = song["language_detect"];
           }
         }
@@ -136,9 +136,9 @@ export class MyComponent {
 
   private convertLengthToInt(): void {
     this.dataObjInt = this.dataObj;
-    for(let i=1; i<=Object.keys(this.dataObjInt).length-1; i++) {
-      if(this.dataObjInt[i].length != undefined) {
-        this.dataObjInt[i].length = parseInt(this.dataObjInt[i].length);    
+    for (let i = 1; i <= Object.keys(this.dataObjInt).length - 1; i++) {
+      if (this.dataObjInt[i].length != undefined) {
+        this.dataObjInt[i].length = parseInt(this.dataObjInt[i].length);
       }
     }
     console.log("dataObjInt -> ")
@@ -147,7 +147,7 @@ export class MyComponent {
 
   private getSongs(dataObj: object) {
     let songs = {};
-    for(let i=1; i<=Object.keys(dataObj).length-1; i++) {
+    for (let i = 1; i <= Object.keys(dataObj).length - 1; i++) {
       songs[i] = this.dataObj[i];
     }
     return songs;
@@ -162,28 +162,28 @@ export class MyComponent {
 
       song["title"] = songs[index]["title"];
 
-      if(songs[index]["language"] != undefined) {
+      if (songs[index]["language"] != undefined) {
         song["language"] = songs[index]["language"];
       }
       else {
         song["language"] = "undefined";
       }
 
-      if(songs[index]["length"] != undefined) {
+      if (songs[index]["length"] != undefined) {
         song["length"] = songs[index]["length"];
       }
       else {
-        song["length"] = "undefined";
+        song["length"] = 0;
       }
 
-      if(songs[index]["format"] != undefined) {
+      if (songs[index]["format"] != undefined) {
         song["format"] = songs[index]["format"][0];
       }
       else {
         song["format"] = "undefined";
       }
 
-      if(songs[index]["genre"] != undefined) {
+      if (songs[index]["genre"] != undefined) {
         song["genre"] = songs[index]["genre"][0];
       }
       else {
@@ -191,6 +191,13 @@ export class MyComponent {
       }
 
       song["isClassic"] = songs[index]["isClassic"];
+      /*if(songs[index]["isClassic"]==true){
+        console.log("je suis dedans")
+        songs[index]["isClassic"]="true"
+      }
+      else{
+        songs[index]["isClassic"]="false"
+      }*/
       songsA.push(song);
     })
 
@@ -208,8 +215,8 @@ export class MyComponent {
       //console.log(obj[index][attribute])
 
       // A way to deal with reading undefined as an object ?
-      if(obj[index][attribute] != undefined) {
-        if(Object.keys(obj[index][attribute]).length <= 1) {
+      if (obj[index][attribute] != undefined) {
+        if (Object.keys(obj[index][attribute]).length <= 1) {
           Object.keys(obj[index][attribute]).forEach(e => {
             value = value + e + " - ";
           });
@@ -249,85 +256,120 @@ export class MyComponent {
     return value
   }
 
-  buildParalleGraph(svg){
-    var margin = {top: 10, right: 10, bottom: 10, left: 0},
-    width = 1000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
-
+  //----- creation du diagramme -------//
+  buildParalleGraph(svg) {
+    var margin = { top: 10, right: 10, bottom: 10, left: 0 },
+      width = 1000 - margin.left - margin.right,
+      height = 1000 - margin.top - margin.bottom;
+    let data1 = this.getDataPhaseA(this.dataObj);
+    console.log("je suis la");
+    console.log(data1);
     svg.append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-  
-  // Parse the Data
-  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then( function(data) {
-console.log(data.Species)
-  // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
- const dimensions = Object.keys(data[0]).filter(function(d) { return d  })
-//console.log(dimensions)
-  // For each dimension, I build a linear scale. I store all in a y object
-  const y = {}
-  for (var i in dimensions) {
-    const name = dimensions[i]
-    if (name !="Species"){
-    y[name] = d3.scaleLinear()// scale point
-      .domain( d3.extent(data, function(d) {return +d[name]; }) ) // 
-      .range([height, 20])
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+    const dimensions = Object.keys(data1[0]).filter(function (d) { return d })
+
+    console.log(dimensions)
+
+    // For each dimension, I build a linear scale. I store all in a y object
+    const y = {}
+
+    const title = [];
+    const format = [];
+    const genre = [];
+    for (var t = 0; t < data1.length; t++) {
+
+      title.push(data1[t].title)
+      format.push(data1[t].format)
+      genre.push(data1[t].genre)
+
+
     }
-    else{
-      y[name] = d3.scalePoint()// scale point
-      .domain( ["setosa","versicolor", "virginica"]/*d3.extent(data, function(d) { console.log(d["Species"]) ;return d["Species"]; }) */) // 
-      .range([height, 20])
+    console.log("je suis la 2")
+    console.log(format)
+    console.log(genre)
+    console.log(title);
+    console.log(data1[1].length)
+
+    for (var i in dimensions) {
+      const name = dimensions[i]
+      if (name == "length") {
+        y[name] = d3.scaleLinear()// scale point
+          .domain(d3.extent(data1, function (d) { return +d[name]; }))
+          .range([height, 20])
+      }
+      else if (name == "title") {
+        y[name] = d3.scalePoint()// scale point
+          .domain(title) // 
+          .range([height, 20])
+      }
+      else if (name == "format") {
+        y[name] = d3.scalePoint()// scale point
+          .domain(format) // 
+          .range([height, 20])
+      }
+      else if (name == "genre") {
+        y[name] = d3.scalePoint()// scale point
+          .domain(genre) // 
+          .range([height, 20])
+      }
+      else {
+        y[name] = d3.scalePoint()// scale point
+          .domain(d3.extent(data1, function (d) { return d[name]; })) // 
+          .range([height, 20])
+      }
+
     }
 
-  }
+    // Build the X scale -> it find the best position for each Y axis
+    const x = d3.scalePoint()
+      .range([0, width])
+      .padding(1)
+      .domain(dimensions);
 
-  // Build the X scale -> it find the best position for each Y axis
- const x = d3.scalePoint()
-    .range([0, width])
-    .padding(1)
-    .domain(dimensions);
+    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+    function path(d) {
+      return d3.line()(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
+    }
+    // Draw the lines
+    svg
+      .selectAll("myPath")
+      .data(data1)
+      .join("path")
+      .attr("d", path)
+      .style("fill", "none")
+      .style("stroke", "#69b3a2")
+      .style("opacity", 0.5)
 
-  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-  function path(d) {
-      return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
-  }
-  console.log(data)
-  // Draw the lines
-  svg
-    .selectAll("myPath")
-    .data(data)
-    .join("path")
-    .attr("d",  path)
-    .style("fill", "none")
-    .style("stroke", "#69b3a2")
-    .style("opacity", 0.5)
+    // Draw the axis:
 
-  // Draw the axis:
- 
-  svg.selectAll("myAxis")
-    // For each dimension of the dataset I add a 'g' element:
-    .data(dimensions).enter()
-    .append("g")
-    // I translate this element to its right position on the x axis
-    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-    // And I build the axis with the call function
-    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-    // Add axis title
-    .append("text")
+    svg.selectAll("myAxis")
+      // For each dimension of the dataset I add a 'g' element:
+      .data(dimensions).enter()
+      .append("g")
+      // I translate this element to its right position on the x axis
+      .attr("transform", function (d) { return "translate(" + x(d) + ")"; })
+      // And I build the axis with the call function
+      .each(function (d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+      // Add axis title
+      .append("text")
       .style("text-anchor", "middle")
       .attr("y", 10)
-      .text(function(d) { return d; })
+      .text(function (d) { return d; })
       .style("fill", "black")
 
-})
+
   }
 
 
   render() {
-    return  (
+    return (
       <Host>
         <h1>Parallel graph</h1>
-        <svg class="chart"/>
+        <svg class="chart" />
       </Host>
     )
   }
