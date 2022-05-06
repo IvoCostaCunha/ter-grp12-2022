7e12f7e6397028d79ba452be6b252836508ead58
omar863 <71570689+omar863@users.noreply.github.com>
2022-05-06 10:56:41 +0200

    trie du length

--- parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
+++ parallel-graph/src/components/parallel-graph/parallel-graph/parallel-graph.tsx
@@ -63,7 +63,7 @@ export class MyComponent {
     let stats = {};
 
     // % missing data
-    
+
     return stats;
   }
 
@@ -205,11 +205,11 @@ export class MyComponent {
       }
 
       song["isClassic"] = songs[index]["isClassic"];
-      if(songs[index]["isClassic"]==true){
-        song["isClassic"]="true"
+      if (songs[index]["isClassic"] == true) {
+        song["isClassic"] = "true"
       }
-      else{
-        song["isClassic"]="false"
+      else {
+        song["isClassic"] = "false"
       }
       songsA.push(song);
     })
@@ -229,14 +229,14 @@ export class MyComponent {
       let nbFormat;
 
       // Choose a type of how to treat data
-      if((songs[index]["genre"] != undefined) && (songs[index]["format"] != undefined)) {
+      if ((songs[index]["genre"] != undefined) && (songs[index]["format"] != undefined)) {
         nbGenre = Object.keys(songs[index]["genre"]).length;
         nbFormat = Object.keys(songs[index]["format"]).length;
 
-        if(nbGenre != 0 && nbFormat !=0) {
+        if (nbGenre != 0 && nbFormat != 0) {
           choice = "format&genre";
         }
-        else choice = "check"; 
+        else choice = "check";
       }
 
       else choice = "undefined detected";
@@ -250,9 +250,9 @@ export class MyComponent {
       song["isClassic"] = songs[index]["isClassic"];
 
       if (choice == "format&genre") {
-        for(let i = 0; i < nbFormat; i++) {
-          for(let j = 0; j < nbGenre; j++) {
-            let songDuplicate = {...song};
+        for (let i = 0; i < nbFormat; i++) {
+          for (let j = 0; j < nbGenre; j++) {
+            let songDuplicate = { ...song };
             songDuplicate["format"] = songs[index]["format"][i];
             songDuplicate["genre"] = songs[index]["genre"][j];
             songsB.push(songDuplicate);
@@ -260,9 +260,9 @@ export class MyComponent {
         }
       }
 
-      else if (choice ==  "equal") {
-        for(let i = 0; i < nbFormat;  i++) {
-          let songDuplicate = {...song};
+      else if (choice == "equal") {
+        for (let i = 0; i < nbFormat; i++) {
+          let songDuplicate = { ...song };
 
           songDuplicate["format"] = songs[index]["format"][i];
           songDuplicate["genre"] = songs[index]["genre"][i];
@@ -271,24 +271,24 @@ export class MyComponent {
         }
       }
       else if (choice == "check") {
-        if(nbGenre != 0) {
-          for(let i = 0; i < nbGenre;  i++) {
-            let songDuplicate = {...song};
-  
+        if (nbGenre != 0) {
+          for (let i = 0; i < nbGenre; i++) {
+            let songDuplicate = { ...song };
+
             songDuplicate["format"] = "undefined";
             songDuplicate["genre"] = songs[index]["genre"][i];
-  
+
             songsB.push(songDuplicate);
-  
+
           }
         }
         else if (nbFormat != 0) {
-          for(let i = 0; i < nbFormat;  i++) {
-            let songDuplicate = {...song};
-  
+          for (let i = 0; i < nbFormat; i++) {
+            let songDuplicate = { ...song };
+
             songDuplicate["format"] = songs[index]["format"][i];
             songDuplicate["genre"] = "undefined";
-  
+
             songsB.push(songDuplicate);
           }
         }
@@ -300,25 +300,25 @@ export class MyComponent {
         }
       }
       else if (choice == "undefined detected") {
-        if((songs[index]["genre"] == undefined) && (songs[index]["format"] != undefined)) {
+        if ((songs[index]["genre"] == undefined) && (songs[index]["format"] != undefined)) {
           nbFormat = Object.keys(songs[index]["format"]).length;
-          for(let i = 0; i < nbFormat;  i++) {
-            let songDuplicate = {...song};
-  
+          for (let i = 0; i < nbFormat; i++) {
+            let songDuplicate = { ...song };
+
             songDuplicate["format"] = songs[index]["format"][i];
             songDuplicate["genre"] = "undefined";
-  
+
             songsB.push(songDuplicate);
           }
         }
-        else if((songs[index]["format"] == undefined) && (songs[index]["genre"] != undefined)) {
+        else if ((songs[index]["format"] == undefined) && (songs[index]["genre"] != undefined)) {
           nbGenre = Object.keys(songs[index]["genre"]).length;
-          for(let i = 0; i < nbGenre;  i++) {
-            let songDuplicate = {...song}; // copy of object and not of reference !
-  
+          for (let i = 0; i < nbGenre; i++) {
+            let songDuplicate = { ...song }; // copy of object and not of reference !
+
             songDuplicate["format"] = "undefined";
             songDuplicate["genre"] = songs[index]["genre"][i];
-  
+
             songsB.push(songDuplicate);
           }
         }
@@ -388,8 +388,8 @@ export class MyComponent {
   //----- creation du diagramme -------//
   buildParalleGraph(svg) {
     var margin = { top: 10, right: 10, bottom: 10, left: 0 },
-      width = 1900  - margin.left - margin.right,
-      height =1000 - margin.top - margin.bottom;
+      width = 1900 - margin.left - margin.right,
+      height = 1000 - margin.top - margin.bottom;
     let data1 = this.getDataPhaseB(this.dataObj);
     //console.log("je suis la");
     //console.log(data1);
@@ -410,8 +410,8 @@ export class MyComponent {
     const longueur = [];
     const format = [];
     const genre = [];
-    const isClassic= [];
-    const language=[]
+    const isClassic = [];
+    const language = []
     for (var t = 0; t < data1.length; t++) {
       longueur.push(data1[t].length)
       title.push(data1[t].title)
@@ -421,6 +421,7 @@ export class MyComponent {
       language.push(data1[t].language)
 
     }
+    longueur.sort()
     //console.log("je suis la 2")
     //console.log(longueur)
     //console.log(format)
@@ -458,10 +459,10 @@ export class MyComponent {
           .domain(isClassic) // 
           .range([height, 20])
       }
-      else {
+      else {
         y[name] = d3.scalePoint()// scale point
-        .domain(language) // 
-        .range([height, 20])
+          .domain(language) // 
+          .range([height, 20])
       }
 
     }
@@ -471,60 +472,60 @@ export class MyComponent {
       .range([0, width])
       .padding(1)
       .domain(dimensions);
-      
-      function addslashes(ch) {
-        ch = ch.replace(/\s+/g, '')
-        ch = ch.replace(/['"]+/g, '')
-        ch = ch.replace(/[^\w\s!?]/g,'')
-        ch = ch.replace(/\?/g,'')
-        return ch.toLowerCase()
-      }
-      
 
-      var mousemove = function(event,d) {
-        const selected_title = addslashes(d)
-        var Tooltip = svg.selectAll("." +selected_title)
-      .append("div")
-      .style("opacity", 1)
-      .attr("class", "tooltip")
-      .style("background-color", "white")
-      .style("border", "solid")
-      .style("border-width", "2px")
-      .style("border-radius", "5px")
-      .style("padding", "5px")
-      .html("The exact value of<br>this cell is: " )
-      .style("right","70px")
-      .style("top","10px")
+    function addslashes(ch) {
+      ch = ch.replace(/\s+/g, '')
+      ch = ch.replace(/['"]+/g, '')
+      ch = ch.replace(/[^\w\s!?]/g, '')
+      ch = ch.replace(/\?/g, '')
+      return ch.toLowerCase()
+    }
+
+
+    var mousemove = function (event, d) {
+      const selected_title = addslashes(d)
+      var Tooltip = svg.selectAll("." + selected_title)
+        .append("div")
+        .style("opacity", 1)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .html("The exact value of<br>this cell is: ")
+        .style("right", "70px")
+        .style("top", "10px")
       //console.log(Tooltip.nodes())
-      }
+    }
 
-      const highlight = function (event, d) {
-       
-        //console.log(addslashes(d))
-        // verifier si c'est un chiffre si c'est un chiffre return
-        const selected_title = addslashes(d)
-       //console.log (selected_title)
-        // first every group turns grey
-        let selection=svg.selectAll(".line")
-          .style("stroke", "#69b3a2")
-          .style("opacity", "0.1")
-          .style("stroke-width", "0.7px")
-        // Second the hovered title takes its red
-        
-          svg.selectAll("." +selected_title)
-          .style("stroke", "#FF0000")
-          .style("opacity", "1")
-          .style("stroke-width", "3px")
-          //console.log(selection.nodes())
-          
-      }
-      const doNotHighlight = function (event) {
-        //console.log("j'ai leave")
-        svg.selectAll(".line")
-          .style("stroke", "#69b3a2")
-          .style("opacity", "1")
-          .style("stroke-width", "0.7px")
-      }
+    const highlight = function (event, d) {
+
+      //console.log(addslashes(d))
+      // verifier si c'est un chiffre si c'est un chiffre return
+      const selected_title = addslashes(d)
+      //console.log (selected_title)
+      // first every group turns grey
+      let selection = svg.selectAll(".line")
+        .style("stroke", "#69b3a2")
+        .style("opacity", "0.1")
+        .style("stroke-width", "0.7px")
+      // Second the hovered title takes its red
+
+      svg.selectAll("." + selected_title)
+        .style("stroke", "#FF0000")
+        .style("opacity", "1")
+        .style("stroke-width", "3px")
+      //console.log(selection.nodes())
+
+    }
+    const doNotHighlight = function (event) {
+      //console.log("j'ai leave")
+      svg.selectAll(".line")
+        .style("stroke", "#69b3a2")
+        .style("opacity", "1")
+        .style("stroke-width", "0.7px")
+    }
     // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
     function path(d) {
       return d3.line()(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
@@ -534,12 +535,12 @@ export class MyComponent {
       .selectAll("myPath")
       .data(data1)
       .join("path")
-        .attr("class", function (d) { return "line " +  addslashes(d.title)})
-        .attr("d", path)
-        .style("fill", "none")
-        .style("stroke", "#69b3a2")
-        .style("opacity", 0.5)
-      
+      .attr("class", function (d) { return "line " + addslashes(d.title) })
+      .attr("d", path)
+      .style("fill", "none")
+      .style("stroke", "#69b3a2")
+      .style("opacity", 0.5)
+
     // Draw the axis:
 
     svg.selectAll("myAxis")
@@ -549,14 +550,14 @@ export class MyComponent {
       // I translate this element to its right position on the x axis
       .attr("transform", function (d) { return "translate(" + x(d) + ")"; })
       // And I build the axis with the call function
-      .each(function (d) {  d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])).selectAll(".tick text").on("mouseover", highlight).on("mousemove", mousemove).on("mouseleave", doNotHighlight) ; })
+      .each(function (d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])).selectAll(".tick text").on("mouseover", highlight).on("mousemove", mousemove).on("mouseleave", doNotHighlight); })
       // Add axis title
       .append("text")
       .style("text-anchor", "middle")
       .attr("y", 10)
-      .text(function (d) {return d; })
+      .text(function (d) { return d; })
       .style("fill", "black")
-      
+
 
 
   }
